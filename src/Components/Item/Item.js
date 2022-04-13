6c03678a8f2e86e481697f4a462052c44b62d186
Amnu2002 <88021987+Amnu2002@users.noreply.github.com>
2022-04-13 16:30:51 +0530

    items students
    items students

---
+++ src/Components/Item/Item.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import './item.css'
+import pepsi from '../../Utils/Images/img-pepsi.png'
+
+export const Item = () => {
+  return (
+      <div className="centbody">
+        <div className="card1">
+        <div className="circle"></div>
+        <div className="content">
+            <h2>Pepsi Cola</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus reiciendis amet minus.</p>
+            <a href="#">Buy Now</a>
+        </div>
+        <img src={pepsi} alt="this is image" className="product-img" />
+    </div>
+    <div className="card1">
+        <div className="circle"></div>
+        <div className="content">
+            <h2>Pepsi Cola</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus reiciendis amet minus.</p>
+            <a href="#">Buy Now</a>
+        </div>
+        <img src={pepsi} alt="this is image" className="product-img" />
+    </div>
+    <div className="card1">
+        <div className="circle"></div>
+        <div className="content">
+            <h2>Pepsi Cola</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus reiciendis amet minus.</p>
+            <a href="#">Buy Now</a>
+        </div>
+        <img src={pepsi} alt="this is image" className="product-img" />
+    </div>
+    <div className="card1">
+        <div className="circle"></div>
+        <div className="content">
+            <h2>Pepsi Cola</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus reiciendis amet minus.</p>
+            <a href="#">Buy Now</a>
+        </div>
+        <img src={pepsi} alt="this is image" className="product-img" />
+    </div>
+    </div>
+  )
+}
--- src/Components/Student/Student.js
+++ src/Components/Student/Student.js
@@ -4,6 +4,50 @@ import './Student.css'
 export const Student = () => {
   return (
     <>
-    </>
+      <div className="row">
+      <div className="card" style={{width: "18rem"}}>
+        <img className="card-img-top" src="https://clipartcraft.com/images/laundry-clipart-woman.png" alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">Washer Man</h5>
+        </div>
+      </div>
+      <div className="card" style={{width: "18rem"}}>
+        <img className="card-img-top" src="https://clipartcraft.com/images/laundry-clipart-woman.png" alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">Confectionary</h5>
+        </div>
+      </div>
+      <div className="card" style={{width: "18rem"}}>
+        <img className="card-img-top" src="https://clipartcraft.com/images/laundry-clipart-woman.png" alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">Medicines</h5>
+        </div>
+      </div>
+      <div className="card" style={{width: "18rem"}}>
+        <img className="card-img-top" src="https://clipartcraft.com/images/laundry-clipart-woman.png" alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">Stationary</h5>
+        </div>
+      </div>
+
+      </div>
+
+      <div className="row">
+      <div className="card" style={{width: "18rem"}}>
+        <img className="card-img-top" src="https://clipartcraft.com/images/laundry-clipart-woman.png" alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">Fruit Shop</h5>
+        </div>
+      </div>
+      <div className="card" style={{width: "18rem"}}>
+        <img className="card-img-top" src="https://clipartcraft.com/images/laundry-clipart-woman.png" alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">Restraunt</h5>
+        </div>
+      </div>
+      </div>
+   
+     
+      </>
   )
 }
