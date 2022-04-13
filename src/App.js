d2fe0b9b90223f6d48dce9e7b2f3e9076bb3b699
Lakshay-prog2503 <79397379+Lakshay-prog2503@users.noreply.github.com>
2022-04-13 07:43:45 +0530

    Adding Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
 import './App.css';
+
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import Home from './Components/Home/Home'
+import Navbar from './Components/Navbar/Navbar'
+import Login from './Components/Login/Login'
+import Signup from './Components/Signup/Signup'
 
 function App() {
   return (
     <>
+    <Navbar />
+    <Router>
+      <Routes>
+        <Route path="/home" element={Home} />
+        <Route path="/login" element={Login} />
+        <Route path="/signup" element={Signup} />
+      </Routes>
+    </Router>
     </>
   );
 }
