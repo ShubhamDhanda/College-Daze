e7d74b3623aac9ca48a42c01bb1efd2caa378505
Lakshay-prog2503 <79397379+Lakshay-prog2503@users.noreply.github.com>
2022-04-13 07:34:17 +0530

    Folder Structure for frontend

---
+++ src/Components/Shopkeeper/Shopkeeper.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Shopkeeper.css'
+
+export const Shopkeeper = () => {
+  return (
+    <>
+    </>
+  )
+}
---
+++ src/Components/Student/Student.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Student.css'
+
+export const Student = () => {
+  return (
+    <>
+    </>
+  )
+}
