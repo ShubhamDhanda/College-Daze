0d8bd374bc491f36d86da2d4edf018e4f7ca55ed
Amnu2002 <88021987+Amnu2002@users.noreply.github.com>
2022-04-14 00:05:35 +0530

    caller
    abc

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,19 +7,6 @@ const Home = () => {
 
   return (
     <>
-    <nav>
-        <label class="logo">College-Dayz</label>
-        <ul><li>
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-            <Link to="/shopkeeper">Shopkeeper</Link>
-            <Link to="active">Home</Link>
-        </li></ul>
-        <label id="icon">
-            <i class="fas fa-bars"></i>
-        </label>
-    </nav>
     </>
   )
 }
---
+++ src/Components/pendingOrders/PendingOrders.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './PendingOrders.css'
+
+const PendingOrders = () => {
+  return (
+      <>
+    <div className='pendings-main'>
+        <span className="serialno">100</span>
+        <span className="Item-Name">Pencil-Box</span>
+        <span className="Quantity">3</span>
+        <span className="Total-Price">60</span>
+        <span className="Userid">Amnu2002</span>
+
+    </div>
+
+
+    </>
+  )
+}
+
+export default PendingOrders
